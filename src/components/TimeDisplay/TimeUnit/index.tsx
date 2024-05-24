da4a97646ffbf719698ad2129fe6867adf8d0a15
Josh Goldberg <git@joshuakgoldberg.com>
2024-05-24 05:11:11 +0300

    feat: add UI for time controls

--- src/components/TimeDisplay/TimeUnit/index.tsx
+++ src/components/TimeDisplay/TimeUnit/index.tsx
@@ -3,23 +3,37 @@ import React from "react";
 import { pluralize } from "../../../utils/plurals.js";
 import styles from "./styles.module.css";
 
+export type OnTimeChange = (increase: number) => void;
+
 export interface TimeUnitProps {
     label: string;
+    onEdit: OnTimeChange | undefined;
     value: number;
 }
 
-export const TimeUnit = ({ label, value }: TimeUnitProps) => {
+export const TimeUnit = ({ label, onEdit, value }: TimeUnitProps) => {
     const labelId = `label-${label}`;
 
     return (
         <span className={styles.timeUnit}>
-            <span
-                aria-labelledby={labelId}
-                aria-live="assertive"
-                className={styles.timeInput}
-            >
-                {value}
-            </span>
+            {onEdit ? (
+                <input
+                    className={styles.timeInput}
+                    defaultValue={value}
+                    onChange={(event) => {
+                        onEdit(event.target.valueAsNumber - value);
+                    }}
+                    type="number"
+                />
+            ) : (
+                <span
+                    aria-labelledby={labelId}
+                    aria-live="assertive"
+                    className={styles.timeInput}
+                >
+                    {value}
+                </span>
+            )}
             <span id={labelId}>{pluralize(label, value)}</span>
         </span>
     );
